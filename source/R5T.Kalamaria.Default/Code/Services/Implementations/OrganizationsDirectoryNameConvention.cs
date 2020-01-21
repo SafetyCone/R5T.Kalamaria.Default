882942f62908dc741d7850ffdbe52e286e68031e
MinexAutomation <minex.automation@gmail.com>
2020-01-20 23:53:19 -0800

    Added default IOrganizationsDirectoryNameConvention implementation.

---
+++ source/R5T.Kalamaria.Default/Code/Services/Implementations/OrganizationsDirectoryNameConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Kalamaria.Default
+{
+    public class OrganizationsDirectoryNameConvention : IOrganizationsDirectoryNameConvention
+    {
+        public const string OrganizationsDirectoryName = "Organizations";
+
+
+        public string GetOrganizationsDirectoryName()
+        {
+            return OrganizationsDirectoryNameConvention.OrganizationsDirectoryName;
+        }
+    }
+}
